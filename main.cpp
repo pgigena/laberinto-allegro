220808c34bc0fac8c5433bcbf65869336a0d6a6f
paulgig125@gmail.com <paulgig125@gmail.com@c61baa59-ba5c-c10a-ef56-8247444c0968>
2011-09-23 18:16:56 +0000

    * Implementada una forma de transición para el movimiento de un tile a otro

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const float FPS = 15;
 const int SCREEN_W = 300;
 const int SCREEN_H = 300;
 const int BOUNCER_SIZE = 30;
+const int CHAR_STEP = 5;
 
 enum MYKEYS {
 	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
@@ -18,6 +19,10 @@ enum MYKEYS {
 
 enum MOUSE_BUTTONS {
 	BTN_LEFT, BTN_RIGHT, BTN_CENTER
+};
+
+enum DIRECTIONS {
+	DIR_NONE, DIR_UP, DIR_RIGHT, DIR_DOWN, DIR_LEFT
 };
 
 // Mapa
@@ -49,8 +54,16 @@ bool key[4] = { false, false, false, false };
 bool mouse[3] = { false, false, false };
 
 // Posición del personaje (En tiles)
-int posTilesX = 0;
-int posTilesY = 0;
+int posTileX = 0;
+int posTileY = 0;
+int posNextTileX = 0;
+int posNextTileY = 0;
+int posX = 0;
+int posY = 0;
+
+bool bIsAdvancing;
+DIRECTIONS charMoveDirection = DIR_NONE;
+DIRECTIONS charFaceDirection = DIR_DOWN;
 
 // Personaje
 ALLEGRO_BITMAP *bouncer = NULL;
@@ -66,7 +79,7 @@ void moverPersonajePredictivo();
 void moverPersonajeCorrectivo();
 void onKeyDown(ALLEGRO_KEYBOARD_EVENT ev);
 void onKeyUp(ALLEGRO_KEYBOARD_EVENT ev);
-
+void animateCharacter();
 
 // Main
 // ------------------------------------------------
@@ -166,8 +179,11 @@ int main(int argc, char **argv)
 		for(a=0; (a<10 && b==0); a++){
 			std::cout << MAPA[i][a] << " ";
 			if(MAPA[i][a] == 2){
-				posTilesX = a;
-				posTilesY = i;
+				posX = a * 30;
+				posY = i * 30;
+
+				posTileX = a;
+				posTileY = i;
 				//b=1;
 			}
 		}
@@ -179,6 +195,9 @@ int main(int argc, char **argv)
 		al_wait_for_event(event_queue, &ev);
 		
 		if(ev.type == ALLEGRO_EVENT_TIMER) {
+			if (bIsAdvancing) {
+				animateCharacter();
+			}
 			//moverPersonajePredictivo();
 			moverPersonajeCorrectivo();
 		}
@@ -195,7 +214,7 @@ int main(int argc, char **argv)
 		if(redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 			pintarMapa(display);
-			al_draw_bitmap(bouncer, (posTilesX * 30), (posTilesY * 30), 0);
+			al_draw_bitmap(bouncer, posX/*(posTileX * 30)*/, posY/*(posTileY * 30)*/, 0);
 			al_flip_display();
 		}
 	}
@@ -241,20 +260,20 @@ void pintarMapa(ALLEGRO_DISPLAY *display) {
 // ------------------------------------------------
 void moverPersonajePredictivo() {
 
-	if(key[KEY_UP] && posTilesY - 1 >= 0 && MAPA[posTilesY-1][posTilesX] != 1) {
-		posTilesY--;
+	if(key[KEY_UP] && posTileY - 1 >= 0 && MAPA[posTileY-1][posTileX] != 1) {
+		posTileY--;
 	}
 
-	if(key[KEY_DOWN] && posTilesY + 1 < MAP_H && MAPA[posTilesY+1][posTilesX] != 1) {
-		posTilesY++;
+	if(key[KEY_DOWN] && posTileY + 1 < MAP_H && MAPA[posTileY+1][posTileX] != 1) {
+		posTileY++;
 	}
 
-	if(key[KEY_LEFT] && posTilesX - 1 >= 0 && MAPA[posTilesY][posTilesX-1] != 1) {
-		posTilesX--;
+	if(key[KEY_LEFT] && posTileX - 1 >= 0 && MAPA[posTileY][posTileX-1] != 1) {
+		posTileX--;
 	}
 
-	if(key[KEY_RIGHT] && posTilesX + 1 < MAP_W && MAPA[posTilesY][posTilesX+1] != 1) {
-		posTilesX++;
+	if(key[KEY_RIGHT] && posTileX + 1 < MAP_W && MAPA[posTileY][posTileX+1] != 1) {
+		posTileX++;
 	}
 
 
@@ -264,27 +283,29 @@ void moverPersonajePredictivo() {
 // moverPersonajeCorrectivo
 // ------------------------------------------------
 void moverPersonajeCorrectivo() {
-	int nXPreliminar = posTilesX;
-	int nYPreliminar = posTilesY;
+	int nXPreliminar = posTileX;
+	int nYPreliminar = posTileY;
 	bool bMoved = false;
 
-	if (key[KEY_UP] && posTilesY - 1 >= 0) {
-		nYPreliminar = posTilesY - 1;
-		bMoved = true;
+	if (charMoveDirection != DIR_NONE) {
+		return;
 	}
 
-	if (key[KEY_DOWN] && posTilesY + 1 < MAP_H) {
-		nYPreliminar = posTilesY + 1;
+	if (key[KEY_UP] && posTileY - 1 >= 0) {
+		nYPreliminar = posTileY - 1;
+		charMoveDirection = DIR_UP;
 		bMoved = true;
-	}
-
-	if (key[KEY_LEFT]  && posTilesX - 1 >= 0) {
-		nXPreliminar = posTilesX - 1;
+	} else if (key[KEY_DOWN] && posTileY + 1 < MAP_H) {
+		nYPreliminar = posTileY + 1;
+		charMoveDirection = DIR_DOWN;
 		bMoved = true;
-	}
-
-	if (key[KEY_RIGHT] && posTilesX + 1 < MAP_W) {
-		nXPreliminar = posTilesX + 1;
+	} else if (key[KEY_LEFT]  && posTileX - 1 >= 0) {
+		nXPreliminar = posTileX - 1;
+		charMoveDirection = DIR_LEFT;
+		bMoved = true;
+	} else if (key[KEY_RIGHT] && posTileX + 1 < MAP_W) {
+		nXPreliminar = posTileX + 1;
+		charMoveDirection = DIR_RIGHT;
 		bMoved = true;
 	}
 
@@ -292,21 +313,69 @@ void moverPersonajeCorrectivo() {
 		int nPosPreliminar = MAPA[nYPreliminar][nXPreliminar];
 		/*
 		std::cout << "_____________________" << endl;
-		std::cout << "ACTUAL X: " << posTilesX << endl;
-		std::cout << "ACTUAL Y: " << posTilesY << endl;
-		std::cout << "TILE ACTUAL: " << MAPA[posTilesX][posTilesY] << endl;
+		std::cout << "ACTUAL X: " << posTileX << endl;
+		std::cout << "ACTUAL Y: " << posTileY << endl;
+		std::cout << "TILE ACTUAL: " << MAPA[posTileX][posTileY] << endl;
 		std::cout << "PRELIMINAR X: " << nXPreliminar << endl;
 		std::cout << "PRELIMINAR Y: " << nYPreliminar << endl;
 		std::cout << "TILE PRELIMINAR: " << nPosPreliminar << endl;
 		*/
 
 		if (nPosPreliminar != 1) {
-			posTilesX = nXPreliminar;
-			posTilesY = nYPreliminar;
-
-			redraw = true;
+			posNextTileX = nXPreliminar;
+			posNextTileY = nYPreliminar;
+		
+			bIsAdvancing = true;
+			//redraw = true;
+		} else {
+			charMoveDirection = DIR_NONE;
 		}
 	}
+}
+
+void animateCharacter() {
+	cout << "______________________" << endl;
+	cout << "posX: " << posX << " | tileX: " << posTileX << endl;
+	cout << "posY: " << posY << " | tileY: " << posTileY << endl;
+
+	double charTilePosX = posX / 30;
+	double charTilePosY = posY / 30;
+
+	if (charTilePosX == posNextTileX && charTilePosY == posNextTileY
+		&& posX % 30 == 0 && posY % 30 == 0) {
+		posTileX = posNextTileX;
+		posTileY = posNextTileY;
+		bIsAdvancing = false;
+		charMoveDirection = DIR_NONE;
+		return;
+	}
+
+	switch (charMoveDirection) {
+		case DIR_UP:
+			posY -= CHAR_STEP;
+			break;
+		case DIR_DOWN:
+			posY += CHAR_STEP;
+			break;
+		case DIR_LEFT:
+			posX -= CHAR_STEP;
+			break;
+		case DIR_RIGHT:
+			posX += CHAR_STEP;
+			break;
+	}
+
+	/*if (charMoveDirection == DIR_UP) {
+		posY -= CHAR_STEP;
+	} else if (charMoveDirection == DIR_DOWN) {
+		posY += CHAR_STEP;
+	} else if (charMoveDirection == DIR_LEFT) {
+		posX -= CHAR_STEP;
+	}else if (charMoveDirection == DIR_RIGHT) {
+		posX += CHAR_STEP;
+	}*/
+
+	redraw = true;
 }
 
 // onKeyDown
@@ -315,18 +384,22 @@ void onKeyDown(ALLEGRO_KEYBOARD_EVENT ev) {
 	switch(ev.keycode) {
 		case ALLEGRO_KEY_UP:
 			key[KEY_UP] = true;
+			charFaceDirection = DIR_UP;
 			break;
 
 		case ALLEGRO_KEY_DOWN:
 			key[KEY_DOWN] = true;
+			charFaceDirection = DIR_DOWN;
 			break;
 
 		case ALLEGRO_KEY_LEFT: 
 			key[KEY_LEFT] = true;
+			charFaceDirection = DIR_LEFT;
 			break;
 
 		case ALLEGRO_KEY_RIGHT:
 			key[KEY_RIGHT] = true;
+			charFaceDirection = DIR_DOWN;
 			break;
 		}
 
@@ -357,18 +430,18 @@ void onKeyUp(ALLEGRO_KEYBOARD_EVENT ev) {
 			break;
 		case ALLEGRO_KEY_ENTER:
 				
-			/*std::cout << "pos x: "<< posTilesX << " pos y: " << posTilesY << " valor actual: "<< MAPA[posTilesX][posTilesY] <<endl
-				<< "\tvalor del que esta arriba: " << MAPA[posTilesY-1][posTilesX]<< endl
-				<< "\tvalor del que esta abajo: " << MAPA[posTilesY+1][posTilesX]<< endl
-				<< "\tvalor del que esta derecha: " << MAPA[posTilesY][posTilesX+1]<< endl
-				<< "\tvalor del que esta izquierda: " << MAPA[posTilesY][posTilesX-1]<< endl
+			/*std::cout << "pos x: "<< posTileX << " pos y: " << posTileY << " valor actual: "<< MAPA[posTileX][posTileY] <<endl
+				<< "\tvalor del que esta arriba: " << MAPA[posTileY-1][posTileX]<< endl
+				<< "\tvalor del que esta abajo: " << MAPA[posTileY+1][posTileX]<< endl
+				<< "\tvalor del que esta derecha: " << MAPA[posTileY][posTileX+1]<< endl
+				<< "\tvalor del que esta izquierda: " << MAPA[posTileY][posTileX-1]<< endl
 			<<endl;*/
 			/*strcat(szMensaje, "X=");
-			_itoa(posTilesX, szBufferCoordenada, 10);
+			_itoa(posTileX, szBufferCoordenada, 10);
 			strcat(szMensaje, szBufferCoordenada);
 
 			strcat(szMensaje, "|Y=");
-			_itoa(posTilesY, szBufferCoordenada, 10);
+			_itoa(posTileY, szBufferCoordenada, 10);
 			strcat(szMensaje, szBufferCoordenada);
 			strcat(szMensaje, "\n");
 
