2645753fcbb6d0bff8cefb942d9dbe2d1363e762
paulgig125@gmail.com <paulgig125@gmail.com@c61baa59-ba5c-c10a-ef56-8247444c0968>
2011-09-24 20:15:49 +0000

    * Implementada la animación de personaje (Muy cabeza) para que mire hacia donde está yendo * Implementado un pintado de mapa desde un tileset muy básico

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 // Constantes varias
-// ------------------------------------------------
-const float FPS = 15;
+// ----------------------------------------------------------------------------
+const float FPS = 60;
 const int SCREEN_W = 300;
 const int SCREEN_H = 300;
-const int BOUNCER_SIZE = 30;
-const int CHAR_STEP = 5;
+const int TILE_SIZE = 30;
+const int CHAR_STEP = 3;	
+const char CHAR_PALETTE[] = "dante_palette.png";//"chars_palette.png";
+const char TILESET_PALETTE[] = "tilesetTest.png";
 
 enum MYKEYS {
 	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
@@ -43,7 +45,7 @@ const int MAPA [MAP_W][MAP_H] = {
 };
 
 // Declaración de variables
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 
 // Flags de pintado y salida
 bool redraw = true;
@@ -52,6 +54,11 @@ bool doexit = false;
 // Estado de teclado y mouse
 bool key[4] = { false, false, false, false };
 bool mouse[3] = { false, false, false };
+
+// Display de la aplicación
+ALLEGRO_DISPLAY *display = NULL;
+
+ALLEGRO_BITMAP *bmpTileset = NULL;
 
 // Posición del personaje (En tiles)
 int posTileX = 0;
@@ -66,26 +73,27 @@ DIRECTIONS charMoveDirection = DIR_NONE;
 DIRECTIONS charFaceDirection = DIR_DOWN;
 
 // Personaje
-ALLEGRO_BITMAP *bouncer = NULL;
+ALLEGRO_BITMAP *bmpChar = NULL;
 // Paleta de personajes
 ALLEGRO_BITMAP *bmpChars = NULL;
 // Código del personaje activo
-int nActiveChar = 1;
+int nActiveSpriteX = 0;
+int nActiveSpriteY = 0;
 
 // Declaración de funciones
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void pintarMapa(ALLEGRO_DISPLAY *display);
 void moverPersonajePredictivo();
 void moverPersonajeCorrectivo();
 void onKeyDown(ALLEGRO_KEYBOARD_EVENT ev);
 void onKeyUp(ALLEGRO_KEYBOARD_EVENT ev);
 void animateCharacter();
+void setCharSprite();
 
 // Main
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
-	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 
@@ -127,29 +135,30 @@ int main(int argc, char **argv)
 	al_set_blender(ALLEGRO_ADD, ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA);
 
 	// Cargo bitmap de la paleta de personajes
-	char szPath[] = "chars_palette.png";
 	al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
-	bmpChars = al_load_bitmap(szPath);
+	bmpChars = al_load_bitmap(CHAR_PALETTE);
 
+	bmpTileset = al_load_bitmap(TILESET_PALETTE);
 	// Creo el bitmap del personaje
-	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-	if(!bouncer) {
-		fprintf(stderr, "failed to create bouncer bitmap!\n");
+	/*bmpChar = al_create_bitmap(TILE_SIZE, TILE_SIZE);
+	if(!bmpChar) {
+		fprintf(stderr, "failed to create bmpChar bitmap!\n");
 		al_destroy_display(display);
 		al_destroy_timer(timer);
 		return -1;
 	}
 
-	al_set_target_bitmap(bouncer);
+	al_set_target_bitmap(bmpChar);
 	al_clear_to_color(al_map_rgba(255, 255, 255, 0));
-	al_draw_bitmap_region(bmpChars, (30 * nActiveChar), 0, BOUNCER_SIZE, BOUNCER_SIZE, 0, 0, 0);
+	al_draw_bitmap_region(bmpChars, (30 * nActiveSprite), 0, TILE_SIZE, TILE_SIZE, 0, 0, 0);
 
-	al_set_target_bitmap(al_get_backbuffer(display));
+	al_set_target_bitmap(al_get_backbuffer(display));*/
+	setCharSprite();
 
 	event_queue = al_create_event_queue();
 	if(!event_queue) {
 		fprintf(stderr, "failed to create event_queue!\n");
-		al_destroy_bitmap(bouncer);
+		al_destroy_bitmap(bmpChar);
 		al_destroy_display(display);
 		al_destroy_timer(timer);
 		return -1;
@@ -175,7 +184,7 @@ int main(int argc, char **argv)
 	int b=0;
 
 	for(i=0; (i < 10 && b==0); i++){
-		std::cout << "\n";
+		std::cout << endl;
 		for(a=0; (a<10 && b==0); a++){
 			std::cout << MAPA[i][a] << " ";
 			if(MAPA[i][a] == 2){
@@ -188,6 +197,8 @@ int main(int argc, char **argv)
 			}
 		}
 	}
+
+	std::cout << endl;
 
 	while(!doexit)
 	{
@@ -214,12 +225,12 @@ int main(int argc, char **argv)
 		if(redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 			pintarMapa(display);
-			al_draw_bitmap(bouncer, posX/*(posTileX * 30)*/, posY/*(posTileY * 30)*/, 0);
+			al_draw_bitmap(bmpChar, posX/*(posTileX * 30)*/, posY/*(posTileY * 30)*/, 0);
 			al_flip_display();
 		}
 	}
 
-	al_destroy_bitmap(bouncer);
+	al_destroy_bitmap(bmpChar);
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
@@ -229,20 +240,23 @@ int main(int argc, char **argv)
 
 
 // pintarMapa
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void pintarMapa(ALLEGRO_DISPLAY *display) {
 	ALLEGRO_BITMAP *tile;
 	int nTileX, nTileY;
 
 	for(int i=0; i < 10 ; i++){
 		for(int a=0; a < 10; a++){
-			tile = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+			tile = al_create_bitmap(TILE_SIZE, TILE_SIZE);
 			al_set_target_bitmap(tile);
 
 			if (MAPA[i][a] == 1){
-				al_clear_to_color(al_map_rgb(255, 0, 0));
+				//al_draw_bitmap(bmpTileset, posX/*(posTileX * 30)*/, posY/*(posTileY * 30)*/, 0);
+				al_draw_bitmap_region(bmpTileset, 30, 0, TILE_SIZE, TILE_SIZE, 0, 0, 0);
+				//al_clear_to_color(al_map_rgb(255, 0, 0));
 			} else {
-				al_clear_to_color(al_map_rgb(0, 255, 0));
+				al_draw_bitmap_region(bmpTileset, 0, 0, TILE_SIZE, TILE_SIZE, 0, 0, 0);
+				//al_clear_to_color(al_map_rgb(0, 255, 0));
 			}
 
 			al_set_target_bitmap(al_get_backbuffer(display));
@@ -257,7 +271,7 @@ void pintarMapa(ALLEGRO_DISPLAY *display) {
 }
 
 // moverPersonajePredictivo
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void moverPersonajePredictivo() {
 
 	if(key[KEY_UP] && posTileY - 1 >= 0 && MAPA[posTileY-1][posTileX] != 1) {
@@ -281,7 +295,7 @@ void moverPersonajePredictivo() {
 }
 
 // moverPersonajeCorrectivo
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void moverPersonajeCorrectivo() {
 	int nXPreliminar = posTileX;
 	int nYPreliminar = posTileY;
@@ -326,6 +340,7 @@ void moverPersonajeCorrectivo() {
 			posNextTileY = nYPreliminar;
 		
 			bIsAdvancing = true;
+			setCharSprite();
 			//redraw = true;
 		} else {
 			charMoveDirection = DIR_NONE;
@@ -333,6 +348,8 @@ void moverPersonajeCorrectivo() {
 	}
 }
 
+// animateCharacter
+// ----------------------------------------------------------------------------
 void animateCharacter() {
 	cout << "______________________" << endl;
 	cout << "posX: " << posX << " | tileX: " << posTileX << endl;
@@ -378,28 +395,83 @@ void animateCharacter() {
 	redraw = true;
 }
 
+// setCharSprite
+// ----------------------------------------------------------------------------
+void setCharSprite() {
+		switch (charFaceDirection)
+		{
+			case DIR_UP:
+				nActiveSpriteX = 3;
+				nActiveSpriteY = 0;
+				break;
+			case DIR_DOWN:
+				nActiveSpriteX = 0;
+				nActiveSpriteY = 0;
+				break;
+			case DIR_LEFT:
+				nActiveSpriteX = 0;
+				nActiveSpriteY = 1;
+				break;
+			case DIR_RIGHT:
+				nActiveSpriteX = 3;
+				nActiveSpriteY = 1;
+				break;
+		}
+
+		if (bmpChar) {
+			al_destroy_bitmap(bmpChar);
+		}
+
+		bmpChar = al_create_bitmap(TILE_SIZE, TILE_SIZE);
+
+		if(!bmpChar) {
+			fprintf(stderr, "failed to create bmpChar bitmap!\n");
+			al_destroy_display(display);
+			//al_destroy_timer(timer);
+			return; //-1;
+		}
+		al_set_target_bitmap(bmpChar);
+		al_clear_to_color(al_map_rgba(255, 255, 255, 0));
+		al_draw_bitmap_region(bmpChars, (30 * nActiveSpriteX), (30 * nActiveSpriteY), TILE_SIZE, TILE_SIZE, 0, 0, 0);
+
+		al_set_target_bitmap(al_get_backbuffer(display));
+		redraw = true;
+}
+
 // onKeyDown
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void onKeyDown(ALLEGRO_KEYBOARD_EVENT ev) {
 	switch(ev.keycode) {
 		case ALLEGRO_KEY_UP:
 			key[KEY_UP] = true;
 			charFaceDirection = DIR_UP;
+			if (!bIsAdvancing) {
+				setCharSprite();
+			}
 			break;
 
 		case ALLEGRO_KEY_DOWN:
 			key[KEY_DOWN] = true;
 			charFaceDirection = DIR_DOWN;
+			if (!bIsAdvancing) {
+				setCharSprite();
+			}
 			break;
 
 		case ALLEGRO_KEY_LEFT: 
 			key[KEY_LEFT] = true;
 			charFaceDirection = DIR_LEFT;
+			if (!bIsAdvancing) {
+				setCharSprite();
+			}
 			break;
 
 		case ALLEGRO_KEY_RIGHT:
 			key[KEY_RIGHT] = true;
-			charFaceDirection = DIR_DOWN;
+			charFaceDirection = DIR_RIGHT;
+			if (!bIsAdvancing) {
+				setCharSprite();
+			}
 			break;
 		}
 
@@ -407,7 +479,7 @@ void onKeyDown(ALLEGRO_KEYBOARD_EVENT ev) {
 
 
 // onKeyUp
-// ------------------------------------------------
+// ----------------------------------------------------------------------------
 void onKeyUp(ALLEGRO_KEYBOARD_EVENT ev) {
 	switch(ev.keycode) {
 		case ALLEGRO_KEY_UP:
@@ -449,21 +521,21 @@ void onKeyUp(ALLEGRO_KEYBOARD_EVENT ev) {
 
 			strcpy(szMensaje, "");*/
 			break;
-		case ALLEGRO_KEY_SPACE:
+		/*case ALLEGRO_KEY_SPACE:
 			if (nActiveChar < 3) {
 				nActiveChar++;
 			} else {
 				nActiveChar = 0;
 			}
-			al_destroy_bitmap(bouncer);
-			bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-			al_set_target_bitmap(bouncer);
+			al_destroy_bitmap(bmpChar);
+			bmpChar = al_create_bitmap(TILE_SIZE, TILE_SIZE);
+			al_set_target_bitmap(bmpChar);
 			al_clear_to_color(al_map_rgba(255, 255, 255, 0));
-			al_draw_bitmap_region(bmpChars, (30 * nActiveChar), 0, BOUNCER_SIZE, BOUNCER_SIZE, 0, 0, 0);
+			al_draw_bitmap_region(bmpChars, (30 * nActiveChar), 0, TILE_SIZE, TILE_SIZE, 0, 0, 0);
 
 			redraw = true;
 
-			break;
+			break;*/
 		case ALLEGRO_KEY_ESCAPE:
 			doexit = true;
 			break;
